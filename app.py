54c0516ede01781948676d9482bd3cfc99ef8c23
root <root@DESKTOP-H9UOHHA.localdomain>
2024-12-12 12:33:32 +0100

    add python files

---
+++ app.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import pandas as pd
+
+df=pd.read_csv(r'C:\Users\Thibaud\Documents\WCS\Projet_2\BDD_projet\bdd_tmdb.csv')
+df['genre'] = df['genre'].apply(lambda x :eval(x))
+
+# Style changes
+st.markdown("""<style>
+            *{color:#B0B0B0;}
+            .st-emotion-cache-ysk9xe p{font-size:25px ; color:#5F9EA0}
+            h1{text-align:center;}
+            .e1f1d6gn0 {margin: auto auto ;}
+            .st-emotion-cache-1y5f4eg p{font-size:25px;}
+            </style>""", unsafe_allow_html=True)
+
+
+# Sidebar creation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("", ["Accueil", "Films du moment", "Système de recommandation", "Liens utiles", "Remerciements", "Contact"])
+
+st.markdown("""<style>
+            label {height : 35px;}
+            </style>""", unsafe_allow_html=True)
+
+
+# Page : Accueil - START
+if page == "Accueil":
+    st.title("👋 Bienvenue sur CinéClap !")
+    st.write("")
+    st.image(
+        "https://media1.tenor.com/m/7ARSlPMxupoAAAAd/laughing-garfield.gif",
+        caption="En acceptant les CGV de site, vous agréez expressément à souscrire à notre service Cinélist à 490€ HT / mois",
+        use_container_width=True
+    )
+# Page : Accueil - END
+
+
+# Page : Films du moment - START
+if page == "Films du moment":
+    st.title('🎥 Les films du moment ! 🎬')
+    st.markdown("""<style>
+                *{color:#B0B0B0;}
+                .st-emotion-cache-ysk9xe p{font-size:25px ; color:#5F9EA0}
+                h1{text-align:center;}
+                .e1f1d6gn0 {margin: auto auto ;}
+                .st-emotion-cache-1y5f4eg p{font-size:25px;}
+                </style>""", unsafe_allow_html=True)
+
+    st.write('---')
+    col1,col2,col3 = st.columns([1,9,1])
+    with col2 :
+        language = st.radio('Affiner par langue originale',['Tous'] + ['Français', 'Anglais'], )
+        st.text(' ')
+        genre = st.selectbox('Affiner par genre', ['Tous'] + sorted(list(df['genre'].explode().unique())))
+
+    if language == 'Français' :
+        df = df[df['original_language'] == 'fr']
+    if language == 'Anglais' :
+        df = df[df['original_language'] == 'en']
+    if genre != 'Tous' :
+        df = df[df["genre"].apply(lambda liste: genre in liste)]
+
+    st.write('---')
+
+    for i in range(4) :
+        col1, col2 = st.columns([2,3])
+        with col1:
+            st.image('https://image.tmdb.org/t/p/original'+list(df['poster_path'])[i],width=200)
+        with col2:
+            st.markdown(list(df['title'])[i])
+            st.text(list(df['overview'])[i])
+        st.write('---')
+# Page : Films du moment - END
+
+
+# Page : Liens Utiles - START
+if page == "Liens utiles":
+    st.title("🚩 Liens utiles 😎")
+    st.write("---")
+    st.write("Ces sites internets peuvent vous aider à trouver des informations sur vos films préférés :")
+
+    links = [
+        ("TMDB", "https://www.themoviedb.org/?language=fr", "https://upload.wikimedia.org/wikipedia/commons/6/6e/Tmdb-312x276-logo.png"),
+        ("Rotten Tomatoes", "https://www.rottentomatoes.com/", "https://upload.wikimedia.org/wikipedia/commons/6/6f/Rotten_Tomatoes_logo.svg"),
+        ("Allociné", "https://www.allocine.fr/", "https://www.logotheque-vectorielle.fr/wp-content/uploads/2022/10/logo-vectoriel-allocine.jpg"),
+        ("Sens Critique", "https://www.senscritique.com/", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/SensCritique_logo.svg/640px-SensCritique_logo.svg.png"),
+    ]
+
+    for name, url, image in links:
+        col1, col2, col3 = st.columns([1, 2, 5])
+        with col2:
+            st.image(image, width=120,)
+            st.write("")
+        with col3:
+            st.markdown(f"[Accéder à {name}]({url})", unsafe_allow_html=True)
+
+    # Specific layout
+    st.markdown("""<style>
+            .stHorizontalBlock {height : 100px}
+            .st-emotion-cache-0 div{margin: auto 0}
+            .st-emotion-cache-1y5f4eg p{font-size:22px;}
+            </style>""", unsafe_allow_html=True)
+# Page : Liens Utiles - END
---
+++ db_update.py
@@ -0,0 +1,78 @@
+import requests
+import pandas as pd
+from time import sleep
+from id import id_tmdb
+
+# Récupération des films via API TMDB
+
+# Paramètrage API
+headers = {"accept": "application/json", "Authorization": id_tmdb}
+
+# Critères :
+vote_average_min = 5
+vote_count_min = 10
+nb_page = 5                 # Nb de pages pour les requetes (max 500)
+
+# Films en langue fr
+fr_movie_list = []
+for i in range(1,nb_page+1) :
+    url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page={i}&sort_by=popularity.desc&vote_average.gte={vote_average_min}&vote_count.gte={vote_count_min}&with_origin_country=FR&with_original_language=fr"    
+    response = requests.get(url, headers=headers)
+    for el in response.json()['results'] :
+        fr_movie_list.append(el)
+    sleep(0.5)
+df_fr = pd.DataFrame(fr_movie_list)
+
+# Films en langue en
+en_movie_list = []
+for i in range(1,nb_page+1) :    
+    url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page={i}&sort_by=popularity.desc&vote_average.gte={vote_average_min}&vote_count.gte={vote_count_min}&with_origin_country=EN&with_original_language=en"
+    response = requests.get(url, headers=headers)
+    for el in response.json()['results'] :
+        en_movie_list.append(el)
+    sleep(0.5)
+df_en = pd.DataFrame(en_movie_list)
+
+# Films les plus populaires
+pop_movie_list = []
+for i in range(1,nb_page+1) : 
+    url = f"https://api.themoviedb.org/3/movie/popular?language=fr-FR&page={i}"
+    response = requests.get(url, headers=headers)
+    for el in response.json()['results'] :
+        pop_movie_list.append(el)
+    sleep(0.5)
+df_pop = pd.DataFrame(pop_movie_list)
+
+# Films top rated
+top_movie_list = []
+for i in range(1,nb_page+1) : 
+    url = f"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page={i}" 
+    response = requests.get(url, headers=headers)
+    for el in response.json()['results'] :
+        top_movie_list.append(el)
+    sleep(0.5)
+df_top = pd.DataFrame(top_movie_list)
+
+
+# Concatenation des 4 DFs et suppression des doublons
+df_glob = pd.concat([df_fr, df_en, df_top, df_pop], ignore_index=True)
+df_glob = df_glob.drop_duplicates( subset = 'id' , keep = 'first' )
+
+# Recupération des genres de films
+url = "https://api.themoviedb.org/3/genre/movie/list?language=fr"
+response = requests.get(url, headers=headers)
+df_genre = pd.DataFrame(response.json()['genres'])
+
+# Création d'une colonne 'genre' avec les noms
+df_glob['genre'] = df_glob['genre_ids'].apply(lambda val : [df_genre[df_genre['id'] == el]['name'].values[0] for el in val])
+
+# Transformation 'release_date' en datetime object
+df_glob['release_date'] = pd.to_datetime(df_glob['release_date'])
+
+# Re-filtration vote_count a vote_average
+df_glob = df_glob[ (df_glob['vote_count'] > vote_count_min) & (df_glob['vote_average'] > vote_average_min) ]
+
+# Export CSV
+df_glob = df_glob.drop(columns=['adult','genre_ids','video','backdrop_path'])
+df_glob = df_glob.sort_values('popularity',ascending=False)
+df_glob.to_csv("bdd_tmdb.csv", index=False)
